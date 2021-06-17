8eaac37ef33cedc691d460975b96eeb6855b9d3f
Drew Davis <davisdre@hotmail.com>
2021-06-17 09:06:09 -0400

    Update get-bitlocker-process.ps1
    Clarify what each line is doing.

--- PowerShell/get-bitlocker-process.ps1
+++ PowerShell/get-bitlocker-process.ps1
@@ -1,9 +1,16 @@
 # PURPOSE: To check the BitLocker Encryption Percentage progress, so you don't have keep running 'Get-BitLockerVolume' to see when encryption has completed. 
 
+# Create a progress bar.
 Do
 {
+    # Get BitLocker Encryption progress.
     $BitLockerProgress = (Get-BitLockerVolume).EncryptionPercentage
+
+    # Create Progress bar.
     Write-Progress -Activity "BitLocker Encryption Progress" -status $BitLockerProgress -PercentComplete $BitLockerProgress
+
+# End progress bar when encryption reaches 100%.
 } Until ($BitLockerProgress -eq 100)
 
+# If Encryption percentage equals 100%, write that drive is bitlocker.
 Write-Host "Drive is BitLocker Encrypted"
