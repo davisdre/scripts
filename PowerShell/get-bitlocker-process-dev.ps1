8271bbad20050d9f4aedc8de03854905eeb10f3d
Drew Davis <davisdre@hotmail.com>
2021-06-17 08:52:28 -0400

    Create get-bitlocker-process-dev.ps1
    created a new version

---
+++ PowerShell/get-bitlocker-process-dev.ps1
@@ -0,0 +1,23 @@
+﻿$BitLockerVolumes = @()
+$BitLockerVolumes = (Get-BitLockerVolume).MountPoint
+
+$BitLockerVolumes.ForEach(
+    {
+        if ((Get-BitLockerVolume).VolumeStatus -eq "FullyEncrypted") {
+            "c: Drive is BitLocker Encrypted"
+        }
+        elseif ((Get-BitLockervolume).VolumeStatus -eq "FullyDecrypted") {
+            "Drive needs to be BitLocker Encrypted"
+        }
+        else
+        {
+            Do
+            {
+                $BitLockerProgress = (Get-BitLockerVolume).EncryptionPercentage
+                Write-Progress -Activity "BitLocker Encryption Progress" -status $BitLockerProgress -PercentComplete $BitLockerProgress
+            } Until ($BitLockerProgress -eq 100)
+
+            Write-Host "Drive is BitLocker Encrypted"
+        }
+    }
+)
