7c2f376b2232ee06e21d3abe304d83c94170a8e8
Drew Davis <davisdre@hotmail.com>
2021-06-17 14:55:29 -0400

    Update get-bitlocker-process-dev.ps1
    Was able to get multi-drives to work.

--- PowerShell/get-bitlocker-process-dev.ps1
+++ PowerShell/get-bitlocker-process-dev.ps1
@@ -1,26 +1,26 @@
 ﻿# PURPOSE: To check the BitLocker Encryption Percentage progress, so you don't have keep running 'Get-BitLockerVolume' to see when encryption has completed. 
 # NOTE - Trying to create a version where I can us if there are multiple mount points on a machine.
 
-$BitLockerVolumes = @()
-$BitLockerVolumes = (Get-BitLockerVolume).MountPoint
+$BitLockerVolumes = Get-BitLockerVolume
 
-$BitLockerVolumes.ForEach(
+$BitLockerVolumes | ForEach-Object{$_.MountPoint}(
     {
-        if ((Get-BitLockerVolume).VolumeStatus -eq "FullyEncrypted") {
-            "c: Drive is BitLocker Encrypted"
+        $BLinfo = Get-BitLockerVolume -MountPoint "$_"
+        if (($BLinfo).VolumeStatus -eq "FullyEncrypted") {
+            "$_ Drive is BitLocker Encrypted"
         }
-        elseif ((Get-BitLockervolume).VolumeStatus -eq "FullyDecrypted") {
-            "Drive needs to be BitLocker Encrypted"
+        elseif (($BLinfo).VolumeStatus -eq "FullyDecrypted") {
+            "$_ Drive needs to be BitLocker Encrypted"
         }
         else
         {
             Do
             {
-                $BitLockerProgress = (Get-BitLockerVolume).EncryptionPercentage
+                $BitLockerProgress = ($BLinfo).EncryptionPercentage
                 Write-Progress -Activity "BitLocker Encryption Progress" -status $BitLockerProgress -PercentComplete $BitLockerProgress
             } Until ($BitLockerProgress -eq 100)
 
-            Write-Host "Drive is BitLocker Encrypted"
+            Write-Host "$_ Drive is BitLocker Encrypted"
         }
     }
 )
