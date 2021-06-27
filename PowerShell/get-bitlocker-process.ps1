b03dda7fc688322936445a4fa6cdb6faa96577c3
Drew Davis <davisdre@hotmail.com>
2021-06-26 21:22:45 -0400

    Update get-bitlocker-process.ps1

--- PowerShell/get-bitlocker-process.ps1
+++ PowerShell/get-bitlocker-process.ps1
@@ -1,30 +1,40 @@
-<#
-.SYNOPSIS
-    Create a progress bar to BitLocker Encryption
-.DESCRIPTION
-    The use of this script to follow allong when BitLocker is in a encryption progress state so you can follow along till it reaches 100% completion.
-.EXAMPLE
-    PS C:\> .\get-bitlocker-process.ps1
-    Checks if drive is in a encryption progress state.
-.INPUTS
-    Inputs (if any)
-.OUTPUTS
-    Output (if any)
-.NOTES
-    This is a as-is, best effort script.
-#>
+# PURPOSE: To check the BitLocker Encryption Percentage progress, so you don't have keep running 'Get-BitLockerVolume' to see when encryption has completed. 
+# NOTE - Trying to create a version where I can us if there are multiple mount points on a machine.
 
-# Create a progress bar.
-Do
-{
-    # Get BitLocker Encryption progress.
-    $BitLockerProgress = (Get-BitLockerVolume).EncryptionPercentage
+# Get BitLocker volumes on machine
+$BitLockerVolumes = Get-BitLockerVolume
 
-    # Create Progress bar.
-    Write-Progress -Activity "BitLocker Encryption Progress" -status $BitLockerProgress -PercentComplete $BitLockerProgress
+# Check the BitLocker status on volumes.
+$BitLockerVolumes | ForEach-Object{$_.MountPoint}(
+    {
+        # Get BitLocker info on drive.
+        $BLinfo = Get-BitLockerVolume -MountPoint "$_"
 
-# End progress bar when encryption reaches 100%.
-} Until ($BitLockerProgress -eq 100)
+        # Check if drive is Bitlocker encrypted.
+        if (($BLinfo).VolumeStatus -eq "FullyEncrypted") {
+            "$_ Drive is BitLocker Encrypted"
+        }
+        
+        # Check if drive is not Bitlocker encrypted. 
+        elseif (($BLinfo).VolumeStatus -eq "FullyDecrypted") {
+            "$_ Drive needs to be BitLocker Encrypted"
+        }
 
-# If Encryption percentage equals 100%, write that drive is bitlocker.
-Write-Host "Drive is BitLocker Encrypted"
+        # If not Encrypted or Decrypted, check if Encryption is in progess on drive.
+        else
+        {
+            # Track progress of enryption.
+            Do
+            {
+                # Get bitlocker encryption precentage
+                $BitLockerProgress = ($BLinfo).EncryptionPercentage
+                # Create progress bar
+                Write-Progress -Activity "BitLocker Encryption Progress" -status $BitLockerProgress -PercentComplete $BitLockerProgress
+              # Close progress bar once it reaches 100%
+            } Until ($BitLockerProgress -eq 100)
+
+            # Write when drive has finish encrypting.
+            Write-Host "$_ Drive is BitLocker Encrypted"
+        }
+    }
+)
